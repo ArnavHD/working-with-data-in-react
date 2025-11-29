adde726eeb62010da04e54e0b57c681dc89342c7
ArnavHD <arnavpaul180@gmail.com>
2025-11-29 12:51:56 -0600

    useref can be used to collect data from an from?

--- src/Components/UncontrolledField/UncontrolledField.jsx
+++ src/Components/UncontrolledField/UncontrolledField.jsx
@@ -2,12 +2,15 @@ import React, { useRef } from 'react';
 
 const UncontrolledField = () => {
     const emailRef = useRef('');
+    const passwordRef = useRef('');
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         // console.log(emailRef); // console.log() হবে {current: input}
         // console.log(emailRef.current); // <input type=" "..../>
         console.log(emailRef.current.value);
+        console.log(passwordRef.current.value);
+        
     } 
 
 
@@ -16,7 +19,7 @@ const UncontrolledField = () => {
             <form onSubmit={handleSubmit}>
                 <input ref={emailRef} type="email" name='email' />
                 <br />
-                <input type="password" name='password' />
+                <input type="password" ref={passwordRef} name='password' />
                 <br />
                 <input type="submit" value="submit" />
             </form>
