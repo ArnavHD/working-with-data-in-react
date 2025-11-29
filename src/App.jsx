47689e3fc176fad62b2c6bea016da0fb22bb355e
ArnavHD <arnavpaul180@gmail.com>
2025-11-29 10:09:02 -0600

    Email value gained using useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import FormAction from './Components/FormAction/FormAction'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
+import UncontrolledField from './Components/UncontrolledField/UncontrolledField'
 import ControlledField from './ControlledField/ControlledField'
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <h1>Explore React Form</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <FormAction></FormAction> */}
-      <ControlledField></ControlledField>
+      {/* <ControlledField></ControlledField> */}
+      <UncontrolledField></UncontrolledField>
       
     </>
   )
