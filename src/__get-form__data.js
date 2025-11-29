ba128d1300c754da1095bede344e56f7941f5f82
ArnavHD <arnavpaul180@gmail.com>
2025-11-29 09:51:50 -0600

    Controlled field

--- src/__get-form__data.js
+++ src/__get-form__data.js
@@ -1,4 +1,12 @@
 /**
  * 1.e.target.[name of input field].value
  * 2. use form action and form data in the action handler. formData.get('name of the input field');
+ * 3.. controlled component. one state per eac field. use state on change on the field. useful to dynamically handle error.
+ * 3.. এভাবেও লেখা যায়। সব handle করতে। 
+ * handle all controlled field in one state object
+ * const [formData, setFormData] = useState({
+ *  name: '',
+ *  password: '',
+ * phone: '',
+ * })
 */
