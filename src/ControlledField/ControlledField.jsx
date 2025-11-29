1820a88f4faf2ca08461be0c3a936a6434bf8bb7
ArnavHD <arnavpaul180@gmail.com>
2025-11-29 09:39:51 -0600

    This will show error message while typing in the password

--- src/ControlledField/ControlledField.jsx
+++ src/ControlledField/ControlledField.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const ControlledField = () => {
 
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -11,19 +12,30 @@ const ControlledField = () => {
     const handlePasswordOnChange = (e)=>{
         // console.log(e.target); // একটা একটা করে click করলে input field টা ততবার দেখাবে। <input .../>
         console.log(e.target.value); // এইবার value দেখা যাবে।  
+        setPassword(e.target.value);
+
+        if(password.length < 6){
+            setError('Password must be 6 character or longer');
+        }
+        else{
+            setError('');
+        }
     }
     return (
         
         // password বা কোনোকিছু ভুল হলে immediately বলবে। 
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="email" name='email' id='' placeholder='Enter Email' required />
+                <input type="email" name='email'  placeholder='Enter Email' required />
                 <br />
                 {/* এখানে value={password} দিলে password fixed করে দিবে field এ তাই কোন password ই type করা যাবে না তাই defaultValue add করা হয়েছে।  */}
-                <input type="password" name='password' onChange={handlePasswordOnChange} defaultValue={password} id='' placeholder='Enter Password' required />
+                <input type="password" name='password' onChange={handlePasswordOnChange} defaultValue={password}  placeholder='Enter Password' required />
                 <br />
                 <input type="submit" value="submit" />
             </form>
+            <p style={{color: "red"}}>
+                <small>{error}</small>
+            </p>
         </div>
     );
 };
