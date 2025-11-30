e74f4907f6e24c4a664fa190b00d18bad9d331dd
ArnavHD <arnavpaul180@gmail.com>
2025-11-29 22:46:07 -0600

    Aunti button increases amount of money

--- src/Components/FamilyTree/Aunt.jsx
+++ src/Components/FamilyTree/Aunt.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Cousine from './Cousine';
+import { MoneyContext } from './FamilyTree';
 
 const Aunt = ({ asset }) => {
+
+    const [money, setMoney] = useContext(MoneyContext);
+
+    const handleAddMoney = () =>{
+        setMoney(money + 5000);
+    }
     return (
         <div>
             <h3>Aunt</h3>
@@ -9,6 +16,7 @@ const Aunt = ({ asset }) => {
                 <Cousine asset={asset} name="Retisha"></Cousine>
                 <Cousine name="Sujoy"></Cousine>
             </section>
+            <button onClick={handleAddMoney}>Add 5000 tk</button>
         </div>
     );
 };
